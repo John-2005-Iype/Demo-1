7022643b2ff41c19e96072eddc1260c7429b4dec
John-2005-Iype <iypedanny@gmail.com>
2025-01-16 09:50:27 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,1 +1,14 @@
-
+#include<conio.h.>
+#include<stdio.h.> 
+#include<iostream.h.> 
+void main() 
+{ 
+    int a,b,c; 
+    clrscr(); 
+    printf("Enter a: "); 
+    scanf("%d",&a); printf("Enter b: "); 
+    scanf("%d",&b); 
+    c=a+b; 
+    printf("Sum= %d",c); 
+    getch(); 
+}
